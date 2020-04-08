05f0928cbff3735fa9ab58d7286b4f3ace275b04
fabianzhafran <fabianzhafransyahharahap@gmail.com>
2020-04-08 11:59:01 +0700

    Nambah GUI, sisa kartu skill dan aura

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,16 +1,14 @@
 package com.avatarduel;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class Controller {
 
     public void btnPressed(Event event) {
         System.out.println("Button pressed!");
     }
+
+
 }
