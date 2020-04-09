f056765154024391f656558b3eda67e77d7f5032
Nathaniel Evan <nathanielevan@zohomail.com>
2020-04-10 06:16:45 +0700

    Changed ArrayList to LinkedList for performance

--- src/main/java/com/avatarduel/Player.java
+++ src/main/java/com/avatarduel/Player.java
@@ -7,30 +7,30 @@ import java.util.*;
 public class Player {
     protected String namePlayer;
     protected int hp;
-    protected ArrayList<Card> deck;
-    protected ArrayList<Card> hand;
-    protected ArrayList<SummonedCharacter> characterOnField;
-    protected ArrayList<Skill> skillOnField;
-    protected ArrayList<Element> land;
+    protected LinkedList<Card> deck;
+    protected LinkedList<Card> hand;
+    protected LinkedList<SummonedCharacter> characterOnField;
+    protected LinkedList<Skill> skillOnField;
+    protected LinkedList<Element> land;
 
     public Player(String namePlayer) {
         this.hp = 100;
         this.namePlayer = namePlayer;
-        deck = new ArrayList<Card>();
+        deck = new LinkedList<Card>();
         /*  take cards from deck to hand.
-            card generation method currently known.
+            card generation method currently unknown.
             will be implemented ASAP.
         */
-        hand = new ArrayList<Card>();
+        hand = new LinkedList<Card>();
         for (int counter = 0; counter < 7; counter++) {
             takeCardFromTop(deck, hand);
         } // takes 7 cards from deck to hand
-        characterOnField = new ArrayList<SummonedCharacter>(); // game starts with empty arena
-        skillOnField = new ArrayList<Skill>(); // see above comment
-        land = new ArrayList<Element>(); // no land powers at start
+        characterOnField = new LinkedList<SummonedCharacter>(); // game starts with empty arena
+        skillOnField = new LinkedList<Skill>(); // see above comment
+        land = new LinkedList<Element>(); // no land powers at start
     }
 
-    public void takeCardFromTop(ArrayList<Card> moveFrom, ArrayList<Card> moveTo) {
+    public void takeCardFromTop(LinkedList<Card> moveFrom, LinkedList<Card> moveTo) {
         int totalCards = moveFrom.size();
         Card getCard = moveFrom.remove(totalCards - 1);
         moveTo.add(getCard);
