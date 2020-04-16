9de79581d657fb324b1e2cbe6b60d8d338c5caa5
fabianzhafran <fabianzhafransyahharahap@gmail.com>
2020-04-16 21:47:19 +0700

    t

--- src/main/java/com/avatarduel/Player.java
+++ src/main/java/com/avatarduel/Player.java
@@ -14,9 +14,9 @@ public class Player {
     protected String namePlayer;
     protected int hp;
     protected Stack<Integer> deck;
-    protected ArrayList<Card> hand;
-    protected ArrayList<SummonedCharacter> characterOnField;
-    protected ArrayList<Skill> skillOnField;
+    protected Card[] hand;
+    protected SummonedCharacter[] characterOnField;
+    protected Skill[] skillOnField;
     protected ElementPower[] elementPower;
 
     public Player(String namePlayer) {
@@ -28,23 +28,23 @@ public class Player {
         for (int i = 0; i < 50; i++) {
             this.deck.push(randomNumber.nextInt(90));
         }
-        hand = new ArrayList<Card>();
-        characterOnField = new ArrayList<SummonedCharacter>();
-        skillOnField = new ArrayList<Skill>();
+        hand = new Card[7];
+        characterOnField = new SummonedCharacter[6];
+        skillOnField = new Skill[];
         elementPower = new ElementPower[4];
         System.out.println(deck.size());
 
     }
 
-    public ArrayList<Card> getHand() {
+    public Card[] getHand() {
         return hand;
     }
     
-    public ArrayList<SummonedCharacter> getCharacterOnField() {
+    public SummonedCharacter[] getCharacterOnField() {
         return characterOnField;
     }
     
-    public ArrayList<Skill> getSkillOnField() {
+    public Skill[] getSkillOnField() {
         return skillOnField;
     }
     
@@ -54,18 +54,18 @@ public class Player {
     
     public void draw() {
 
-        System.out.println("~~ " + this.namePlayer + " Draw : ");
+        // System.out.println("~~ " + this.namePlayer + " Draw : ");
         ListOfCards listOfCards = new ListOfCards();
         boolean found = false;
         ElementDictionary elementDictionary = new ElementDictionary();
         if (deck.size() > 0) {
             int topCardId = deck.pop();
-            System.out.println("top card id : ");
-            System.out.println(topCardId);
+            // System.out.println("top card id : ");
+            // System.out.println(topCardId);
             for (String[] landRow : listOfCards.listOfLandCards) {
                 System.out.println(landRow[0]);
                 if (topCardId == Integer.parseInt(landRow[0])) {
-                        System.out.println("~~ Found a land card.");
+                        // System.out.println("~~ Found a land card.");
                         Land landCard = new Land(landRow[1], 
                                              elementDictionary.getElement(landRow[2]), 
                                              landRow[3], 
@@ -80,7 +80,7 @@ public class Player {
             if (!found) {
                 for (String[] monsterRow : listOfCards.listOfMonsterCards) {
                     if (topCardId == Integer.parseInt(monsterRow[0])) {
-                        System.out.println("~~ Found a monster card.");
+                        // System.out.println("~~ Found a monster card.");
                         Monster monsterCard = new Monster(monsterRow[1], 
                                                            elementDictionary.getElement(monsterRow[2]), 
                                                            monsterRow[3], 
@@ -99,7 +99,7 @@ public class Player {
             if (!found) {
                 for (String[] skillAuraRow : listOfCards.listOfSkillAuraCards) {
                     if (topCardId == Integer.parseInt(skillAuraRow[0])) {
-                        System.out.println("~~ Found a skillAura card.");
+                        // System.out.println("~~ Found a skillAura card.");
                         Aura skillAuraCard = new Aura(skillAuraRow[1], 
                                                       elementDictionary.getElement(skillAuraRow[2]), 
                                                       skillAuraRow[3], 
@@ -118,6 +118,6 @@ public class Player {
         
     }
 
-    
+
 
 }
