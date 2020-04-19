1caa32ee918fa765f18f9aa7b00f0f8bfe86bb8a
Rainburn <cyber221168@yahoo.com>
2020-04-20 02:57:36 +0700

    nambahin buat test create skill

--- src/test/java/com/testing/CardTest.java
+++ src/test/java/com/testing/CardTest.java
@@ -9,7 +9,7 @@ public class CardTest {
 
     @Test
     public void testCreateMonster() {
-        Monster dummy = new Monster("Fabian", ELEMENT.WATER, "Jon ketiduran", 10, 10, 10);
+        Monster dummy = new Monster("Fabian", Element.WATER, "Jon ketiduran", "tidak di root", 10, 10, 10);
         assertEquals("Name must be Fabian", "Fabian", dummy.getName());
         assertEquals("Attack value must be 10", 10, dummy.getAttackValue());
         assertEquals("Defense value must be 10", 10, dummy.getDefenseValue());
@@ -19,17 +19,17 @@ public class CardTest {
 
     @Test
     public void testCreateLand() {
-        Land dummy = new Land("Bandung", ELEMENT.FIRE, "Bandung Lautan Api", "/usr/bin/local/python");
+        Land dummy = new Land("Bandung", Element.FIRE, "Bandung Lautan Api", "/usr/bin/local/python");
         assertEquals("Name must be Bandung", "Bandung", dummy.getName());
-        assertEquals("Element must be Fire", ELEMENT.FIRE, dummy.getElement());
+        assertEquals("Element must be Fire", Element.FIRE, dummy.getElement());
         assertEquals("Description must be Bandung Lautan Api", "Bandung Lautan Api", dummy.getDescription());
     }
 
     @Test
     public void testCreateAura() {
-        Aura dummy = new Aura("Negative", ELEMENT.AIR, "Aura negatif gan", "/usr/bin/avatar", 10, 10, 10);
+        Aura dummy = new Aura("Negative", Element.AIR, "Aura negatif gan", "/usr/bin/avatar", 10, 10, 10);
         assertEquals("Name must be Negative", "Negative", dummy.getName());
-        assertEquals("Element must be Air", ELEMENT.AIR, dummy.getElement());
+        assertEquals("Element must be Air", Element.AIR, dummy.getElement());
         assertEquals("Attack value must be 10", 10, dummy.getAttackValue());
         assertEquals("Power value must be 10", 10, dummy.getPowerValue());
 
@@ -37,20 +37,28 @@ public class CardTest {
 
     @Test
     public void testCreateDestroy() {
-        Destroy dummy = new Destroy("noob slayer", ELEMENT.FIRE, "noob slayer", "root", 10);
+        Destroy dummy = new Destroy("noob slayer", Element.FIRE, "noob slayer", "root", 10);
         assertEquals("Name must be noob slayer", "noob slayer", dummy.getName());
-        assertEquals("Element must be Fire", ELEMENT.FIRE, dummy.getElement());
+        assertEquals("Element must be Fire", Element.FIRE, dummy.getElement());
         assertEquals("Path must be root", "root", dummy.getImagePath());
 
     }
 
     @Test
     public void testCreatePowerUp() {
-        PowerUp dummy = new PowerUp("nanda", ELEMENT.EARTH, "fakboi if", "anandayulizar", 10);
+        PowerUp dummy = new PowerUp("nanda", Element.EARTH, "fakboi if", "anandayulizar", 10);
         assertEquals("Name must be nanda", "nanda", dummy.getName());
         assertEquals("desc must be fakboi if", "fakboi if", dummy.getDescription());
         assertEquals("Power must be 10", 10, dummy.getPowerValue());
 
+    }
+
+    @Test
+    public void testCreateSkill()) {
+        Skill dummy = new Skill("Coding", Element.WATER, "Ini skill gan", "~", 5);
+        assertEquals("Nama must be Coding", "Coding", dummy.getName());
+        assertEquals("Type must be Skill", "Skill", dummy.getType());
+        assertEquals("Power value must be 5", 5, dummy.getPowerValue());
     }
     
 }
